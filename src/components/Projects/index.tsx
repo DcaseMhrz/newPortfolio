b8ab4d47c7bc77687d7da988a14aa58b6ee5d076
Dikesh <dcasemhrz@gmail.com>
2024-06-11 01:52:57 +1000

    final updates

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,4 +1,4 @@
-import RecentProjects from "../Homepage/RecentProjects"
+
 import ProjectList from "./ListAllProjects"
 
 interface props {
@@ -9,10 +9,10 @@ const Projects: React.FC<props> = ({ darkMode }) => {
         <div className="bg-white col-span-2 row-span-3 rounded-2xl shadow-lg p-4 dark:bg-gray-800 dark:text-gray-200">
             
             <div className="  flex justify-between space-x-12 p-4">
-                <h3 className="text-5xl font-semibold dark:text-gray-200 mb-4 ">
-                Check Out My Latest Projects
+                <h3 className="text-4xl font-semibold dark:text-gray-200 mb-4 ">
+                Check Out My <span className="text-blue-500 font-bold">Latest Projects</span>
                 </h3>
-                <h2 className="text-2xl  w-11/12 font-light ">  I'm here to help if you're searching for a developer bring your idea to life or a devops engineer to help take your business to the next level.</h2>
+                <h2 className="text-xl  w-11/12 font-light ">  I'm here to help if you're searching for a <span className="text-blue-500 font-bold">developer</span> bring your idea to life or a <span className="text-blue-500 font-bold">devops engineer</span> to help take your <u>business to the next level.</u></h2>
             </div>
             <ProjectList darkMode={darkMode}/>
 
